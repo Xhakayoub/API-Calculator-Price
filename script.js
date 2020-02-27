e6942a4179a6d9f5508d40929a10394bb4f50daf
Xhakayoub <mouissi.ayoub@gmail.com>
2020-02-27 01:26:26 +0100

    final commit

--- script.js
+++ script.js
@@ -1,25 +1,96 @@
 const express = require('express');
+var bodyParser = require('body-parser');
+
 
 const app = express();
-const route = express.Router();
-
 const PORT = process.env.PORT || 3000;
 
-var message = "hello ayoub";
+// Pour supporter JSON-encoded 
+// Pour supporter URL-encoded 
+app.use( bodyParser.json() );       
+app.use(bodyParser.urlencoded({     
+  extended: true
+})); 
+///////////////////////////////
 
+//  modifier le header du http Pour Annuler le mécanisme qui permet à des ressources restreintes d'une page web, 
+//  d'être récupérées par un autre domaine extérieur au domaine à partir duquel la première ressource a été servie
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
    next();
-  });
+  });    
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
-app.get('/api', (req, res) => {
-    res.send(message);
-    console.log(req.host);
+
+  app.post('/calcule', (req, res) => {
+  
+   var primaryPrice = req.body.price * req.body.area;
+   var finalPrice = primaryPrice;
+   
+//    console.log(`prix : ${req.body.price}, surface : ${req.body.area}, ndp : ${req.body.numberOfRooms}, 
+//    type : ${req.body.type}, etat : ${req.body.status}, `);
+//    console.log(`prix de depart est ${primaryPrice}`);
+
+
+
+//  la verification de la valeur nombre de pieces, et le calcule si la valeur existe
+   if (typeof req.body.numberOfRooms != 'undefined'){
+       
+       var nor = req.body.numberOfRooms;
+       
+          if(nor <= 2) finalPrice = finalPrice + (primaryPrice*0.05);
+                 
+          else if (nor == 3 || nor ==4) finalPrice = finalPrice + (primaryPrice*0.02);
+             
+          else if (nor<=5) finalPrice = finalPrice - (primaryPrice*0.01);
+               
+   }
+//   console.log(`prix apres le calcule en focntion de nombre de pieces ${finalPrice}`);
+//////////////////////////////////////////////////////////////////////////////////////////
+
+
+//  la verification de la valeur type, et le calcule si la valeur existe
+   if (typeof req.body.type != 'undefined'){
+    var type = req.body.type;
+    switch(type){
+        case ('Maison'):
+            finalPrice = finalPrice + (primaryPrice*0.03);
+            break;
+        case ('Appartement'):
+            finalPrice = finalPrice + (primaryPrice*0.05);
+            break;
+        default:{}
+       }
+   }
+//   console.log(`prix apres le calcule en focntion de type de bien ${finalPrice}`);
+/////////////////////////////////////////////////////////////////////////////////////
+
+
+//  la verification de la valeur etat du bien, et le calcule si la valeur existe
+   if (typeof req.body.status != 'undefined'){
+    var status = req.body.status;
+    switch(status){
+        case ("L'état du bien necessite des travaux"):
+            finalPrice = finalPrice - (primaryPrice*0.1);
+            break;
+        case ("Refait à neuf"):
+            finalPrice = finalPrice + (primaryPrice*0.12);
+            break;
+        default:{}
+       }
+   }
+ //  console.log(`prix apres le calcule en fonction de nombre de pieces ${finalPrice}`);
+ ////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+  // L'envoie de notre prix calcule 
+   res.setHeader('Content-Type', 'application/json');
+   res.send({ data: finalPrice })
+   ///////////////////////////////////////////////////
+   
 })
 
-route.route('/toto').post((req, res) => {
-    res.send(req);
-})
 
 app.listen( PORT, () => { console.log(`Running on port ${ PORT }`) });
